e36d4a3b756a051a7e4f39782267fea1638ba5e4
Leonardo Cintra <leonardo.ncintra@outlook.com>
2017-04-26 19:19:10 -0300

    configurando webpack para react

---
+++ exercicio_react/webpack.config.js
@@ -0,0 +1,23 @@
+const webpack = require('webpack')
+
+module.exports = {
+    entry: './ex/index.js',
+    output: {
+        path: __dirname + '/public',
+        filename: './bundle.js'
+    },
+    devServer: {
+        port: 3000,
+        contentBase: './public',
+    },
+    module: {
+        loaders: [{
+            test: /.jsx?$/,
+            loader: 'babel-loader',
+            exclude: /node_modules/,
+            query: {
+                presets: ['react']
+            }
+        }]
+    }
+}
