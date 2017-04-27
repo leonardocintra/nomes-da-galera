4d3ffb9d620415f5287cec71e7215ed9cd0341c2
Leonardo Cintra <leonardo.ncintra@outlook.com>
2017-04-26 22:24:54 -0300

    state do compoment

--- exercicio_react/ex.8/index.jsx
+++ exercicio_react/ex.8/index.jsx
@@ -4,6 +4,6 @@ import ReactDOM from 'react-dom'
 import ClassComponent from './classComponent'
 
 ReactDOM.render(
-    <ClassComponent label='Contador' initialValue={10} />
+    <ClassComponent value="Ronaldo Bria muito no curintia" />
     , document.getElementById('app')
 )
