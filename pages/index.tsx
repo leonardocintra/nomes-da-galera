ab70a0ea5fa97132340334784e9511b286875404
Leonardo Cintra <leonardo.ncintra@outlook.com>
2022-03-22 17:36:50 -0300

    tabela

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,20 @@
 import Layout from "../components/Layout";
+import Tabela from "../components/Tabela";
+import Cliente from "../model/Cliente";
 
 export default function Home() {
+
+  const clientes = [
+    new Cliente('Ana', 23, '1'),
+    new Cliente('Maria', 25, '1'),
+    new Cliente('Juliana', 43, '1'),
+    new Cliente('Ronaldo', 13, '1')
+  ]
+
   return (
     <div className="flex justify-center items-center h-screen bg-gradient-to-r from-blue-500 to-purple-500 text-white">
       <Layout titulo="Cadastro Simples">
-        <span>Conteudo</span>
+        <Tabela clientes={clientes} />
       </Layout>
     </div>
   )
