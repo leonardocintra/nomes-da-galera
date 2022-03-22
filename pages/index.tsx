c78dee88d46199526274fcad012d730991197cb7
Leonardo Cintra <leonardo.ncintra@outlook.com>
2022-03-22 16:58:38 -0300

    primeiro componentes

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,11 @@
+import Layout from "../components/Layout";
+
 export default function Home() {
   return (
-    <div className="flex h-screen justify-center items-center bg-gradient-to-tr from-purple-500 to-blue-600 via-yellow-500">
-      <span className="text-4xl">Texto qualquer</span>
-
+    <div className="flex justify-center items-center h-screen bg-gradient-to-r from-blue-500 to-purple-500 text-white">
+      <Layout titulo="Cadastro Simples">
+        <span>Conteudo</span>
+      </Layout>
     </div>
   )
 }
