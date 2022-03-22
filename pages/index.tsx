4531faa28833bdfb81eaa386f4854f85737e9573
Leonardo Cintra <leonardo.ncintra@outlook.com>
2022-03-22 16:29:32 -0300

    Reinitial Project

---
+++ pages/index.tsx
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <div className="flex h-screen justify-center items-center bg-gradient-to-tr from-purple-500 to-blue-600 via-yellow-500">
+      <span className="text-4xl">Texto qualquer</span>
+
+    </div>
+  )
+}
